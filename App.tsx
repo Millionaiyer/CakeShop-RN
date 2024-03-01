dc9825d4a9b6ca7952311b03d9f2425c15956503
Millionaiyer <surajsaiyer@gmail.com>
2024-03-01 14:51:02 +0530

    status bar and header completed

--- App.tsx
+++ App.tsx
@@ -1,16 +1,17 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 import Home from "./pages/home";
 import Profile from "./pages/profile";
 import Category from "./pages/category";
 import Header from "./layout/header/header";
 import Footer from "./layout/footer/footer";
+import Status from "./layout/statusbar/statusbar";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      {/* <Header /> */}
-      <Home />
+    <View style={styles.main}>
+      <Status />
+      <Header />
+      {/* <Home /> */}
       {/* <Profile /> */}
       {/* <Category /> */}
       {/* <Footer /> */}
@@ -19,10 +20,7 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  main: {
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
